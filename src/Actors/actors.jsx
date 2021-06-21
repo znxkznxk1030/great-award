80ebcd02a71dc5df7ec6a2ddd7032002fe62969f
ysKim <znxkznxk1030@gmail.com>
2021-06-22 00:56:52 +0900

    insert pic

--- src/Actors/actors.jsx
+++ src/Actors/actors.jsx
@@ -1,14 +1,28 @@
 import React from "react";
 import "./style-actors.scss";
 
+import { actors } from "../mock-data/actors";
+
 class Actors extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      actors,
+    };
+  }
+
   render() {
     return (
-      <main class="actors-background">
-        <div>
-          <h1>actors</h1>
-        </div>
-      </main>
+      <div className="actors-background">
+        {this.state.actors.map((actor, index) => {
+          return (
+            <div className="actor-frame" key={index}>
+              <img src={actor.src} alt={actor.name} className={"actor-img-" + index}/>
+              <p>{actor.name}</p>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 }
---
+++ src/mock-data/actors.js
@@ -0,0 +1,28 @@
+const actors = [
+  {
+    name: "강 호 동",
+    src: "https://assets.codepen.io/1328906/kang-001_1.png"
+  },
+  {
+    name: "김 종 민",
+    src: "https://assets.codepen.io/1328906/min-001.png"
+  },
+  {
+    name: "김 동 현",
+    src: "https://assets.codepen.io/1328906/hyun-001.png"
+  },
+  {
+    name: "신 동",
+    src: "https://assets.codepen.io/1328906/dong-001.png"
+  },
+  {
+    name: "유 병 재",
+    src: "https://assets.codepen.io/1328906/you-001png.png"
+  },
+  {
+    name: "피 오",
+    src: "https://assets.codepen.io/1328906/pio-001.png"
+  },
+];
+
+export { actors };
