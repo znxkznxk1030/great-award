f1716bd33bfa82040ba2f99577d58aebbb190a06
ysKim <znxkznxk1030@gmail.com>
2021-06-26 00:25:54 +0900

    Home | star wars text effect

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -14,8 +14,49 @@ class Home extends React.Component {
   render() {
     return (
       <div className="home-background">
-        <div className="title">
+        <div className="title--header">
           <p>대탈출 팬 사이트</p>
+        </div>
+
+        <div className="context star-wars">
+          <div className="crawl">
+            <h1> 대 탈 출 4 </h1>
+            <h2> 7 / 11 첫 방 송 </h2>
+            <div className="text--inline">
+              <p className="text--position">감독</p>
+              <p className="text--name">정종연</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--position">미술</p>
+              <p className="text--name">장연옥</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--position">출연</p>
+              <p className="text--name">강호동</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--name">김종민</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--name">김동현</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--name">신동</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--name">유병재</p>
+            </div>
+            <div className="text--inline">
+              <p className="text--name">피오</p>
+            </div>
+          </div>
+          {this.state.actorBgImgList.map((actor, index) => (
+            <img
+              src={actor.src}
+              alt="404"
+              className={"actor-img-" + index}
+            ></img>
+          ))}
         </div>
         <div className="video-wrapper">
           <iframe
@@ -26,15 +67,6 @@ class Home extends React.Component {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
           ></iframe>
-        </div>
-        <div className="context">
-          {this.state.actorBgImgList.map((actor, index) => (
-            <img
-              src={actor.src}
-              alt="404"
-              className={"actor-img-" + index}
-            ></img>
-          ))}
         </div>
       </div>
     );
